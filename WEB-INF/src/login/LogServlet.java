cdf080de1327f3e07fe9de6f6bad6ac5d0c91d70
kiharashunsuke <p2406.s-kihara@uknet.co.jp>
2024-05-10 13:45:52 +0900

    Login更新

--- WEB-INF/src/login/LogServlet.java
+++ WEB-INF/src/login/LogServlet.java
@@ -14,7 +14,7 @@ public class LogServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	// ログイン画面へ遷移
-    	RequestDispatcher rd = request.getRequestDispatcher("/intro/log.jsp");
+    	RequestDispatcher rd = request.getRequestDispatcher("/Login/log.jsp");
     	rd.forward(request, response);
 	}
 
@@ -42,7 +42,7 @@ public class LogServlet extends HttpServlet{
     				result.setLogin_user_flg(1);
     				// ログイン画面に返す
     				request.setAttribute("result", result);
-    				request.getRequestDispatcher("/intro/log.jsp").forward(request, response);
+    				request.getRequestDispatcher("/Login/log.jsp").forward(request, response);
     			} else {
     				// ログインユーザ用にBeanクラスをインスタンス
     				LoginBean result_user = new LoginBean();
@@ -60,7 +60,7 @@ public class LogServlet extends HttpServlet{
     		}catch(Exception e){
     			//何らかの理由で失敗したらエラーページにエラー文を渡して表示。
                 request.setAttribute("error", e.getMessage());
-                request.getRequestDispatcher("/intro/logfailed.jsp").forward(request, response);
+                request.getRequestDispatcher("/Login/logError.jsp").forward(request, response);
     		}
     	}
 
