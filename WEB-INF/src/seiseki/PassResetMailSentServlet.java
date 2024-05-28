3eec4cc724bd7b4b9ac7f47d7955a12455d21420
kiharashunsuke <p2406.s-kihara@uknet.co.jp>
2024-05-28 15:32:44 +0900

    パスワード変更失敗更新

--- WEB-INF/src/seiseki/PassResetMailSentServlet.java
+++ WEB-INF/src/seiseki/PassResetMailSentServlet.java
@@ -1,32 +1,54 @@
 package seiseki;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public class PassResetMailSentServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("Windows-31J");
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 
 		String forwardURL = null;
 		try {
+			Class.forName("org.postgresql.Driver");
+			conn = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/logindb",
+					//"jdbc:postgresql://52.195.46.205:5432/seiseki_database",
+					"postgres", "postgres");
+			ps = conn.prepareStatement("SELECT * FROM LOGIN WHERE PASSWORD = ?");
+			ps.setString(1, password);
+			rs = ps.executeQuery();
+			boolean bo = rs.next();
 			PassResetMailBean pp = new PassResetMailBean(id, password);
 			int updateCount = PassResetDAO.update(pp);
-			if(updateCount < 1) {
-				forwardURL = "/seiseki/passReset.jsp";
+			if(updateCount < 1 || bo ) {
+				forwardURL = "/seseki/passResetMailError.jsp";
 			}else {
 				forwardURL = "/seiseki/log.jsp";
 			}
 		}catch(NumberFormatException e) {
         		e.printStackTrace();
-        		forwardURL = "/seiseki/passReset.jsp";
-        }
+        		forwardURL = "/seiseki/passResetMailError.jsp";
+        } catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			forwardURL = "/seiseki/passResetMailError.jsp";
+		} catch (SQLException e) {
+			e.printStackTrace();
+			forwardURL = "/seiseki/passResetMailError.jsp";
+		}
         	request.getRequestDispatcher(forwardURL).forward(request, response);
 
 	}
---
+++ seiseki/passResetMailError.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=windows-31j"
+    pageEncoding="windows-31j"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-31j">
+<title>パスワード変更失敗</title>
+</head>
+<body>
+
+<font color="red" face="ＭＳ Ｐ明朝,ＭＳ 明朝">パスワード変更に失敗しました。</font>
+<br><br>
+<a href="<%=request.getContextPath() %>/seiseki/passReset.jsp">メール送信画面に戻る
+</a>
+</body>
+</html>
